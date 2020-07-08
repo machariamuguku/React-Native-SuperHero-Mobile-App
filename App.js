d2db9529eda9925656a2d5675a68ddf8bc3bf9db
Muguku <hello@muguku.co.ke>
2020-07-08 22:22:45 +0300

    hide splash screen on initial render

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import {Router} from './router';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return <Router />;
 }
