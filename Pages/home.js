274aabca97b22dc4a88129cf22f864e317c61397
Muguku <hello@muguku.co.ke>
2020-07-08 15:15:41 +0300

    add scrollbar,activity indicator,refresh control, and alert,implement api integration

--- Pages/home.js
+++ Pages/home.js
@@ -5,10 +5,18 @@ import {
   TextInput,
   TouchableOpacity,
   Text,
+  ScrollView,
+  RefreshControl,
+  ActivityIndicator,
+  Alert,
 } from 'react-native';
 
-import {Sample} from '../sample';
+import {useQuery} from 'react-query';
+
 import {HomeCard} from '../Components/homeCard';
+
+// utility functions to query API
+import {generateSixRandom, searchHero} from '../utils/fetchApi';
 
 const styles = StyleSheet.create({
   container: {
@@ -47,10 +55,11 @@ const styles = StyleSheet.create({
   },
   mainContainer: {
     flex: 8,
-    justifyContent: 'center',
-    alignContent: 'center',
   },
   cardHolderContainer: {
+    flex: 1,
+  },
+  scrollView: {
     flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -59,22 +68,79 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   cardHolder: {
-    width: '46%',
-    height: '26%',
+    width: 165,
+    height: 113,
   },
 });
 
 export function Home({navigation}) {
-  const [superHeroes, setSuperHeroes] = useState([
-    Sample,
-    Sample,
-    Sample,
-    Sample,
-    Sample,
-    Sample,
-  ]);
   const [superHeroName, setSuperHeroName] = useState('Find Your SuperHero');
+  const [superHeroData, setSuperHeroData] = useState([]);
+  const [superHeroFetchError, setSuperHeroFetchError] = useState('');
 
+  const [isReFetching, setIsReFetching] = useState(false);
+
+  // fetch 6 random superheroes to populate the landing page
+  const {
+    status: superHeroesStatus,
+    data: superHeroesData,
+    isFetching: isFetchingSuperHeroes,
+    refetch,
+  } = useQuery('superHeroes', async () => {
+    const fetchData = await generateSixRandom();
+    return fetchData;
+  });
+
+  // refetch on pull down
+  const invalidateAndRefetch = () => {
+    setIsReFetching(true);
+    setSuperHeroData([]);
+    setSuperHeroFetchError('');
+    refetch().then(() => setIsReFetching(false));
+  };
+
+  // search one hero by name
+  async function searchByName(name) {
+    setIsReFetching(true);
+    await searchHero(name).then(fetchData => {
+      if (fetchData.response === 'success') {
+        setSuperHeroFetchError('');
+        if (fetchData.results.length > 6) {
+          let trimmedData = fetchData.results.filter(
+            (_, index) => !(index > 5),
+          );
+          createButtonAlert();
+          setSuperHeroData(trimmedData);
+          setIsReFetching(false);
+          return;
+        }
+        setSuperHeroData(fetchData.results);
+        setIsReFetching(false);
+        return;
+      }
+      setSuperHeroFetchError(fetchData.error);
+      setIsReFetching(false);
+      return;
+    });
+    setIsReFetching(false);
+    return;
+  }
+
+  // programmatically toggle-able alert
+  const createButtonAlert = () =>
+    Alert.alert(
+      'Warning!',
+      'The result set for this query is too large. Please enter a narrowed down search term. Showing only the first 6',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+      ],
+      {cancelable: false},
+    );
+
+  // programmatically navigate to profile page
   const navigateToProfile = superHero =>
     navigation.navigate('Profile', {
       hero: superHero,
@@ -95,9 +161,7 @@ export function Home({navigation}) {
           <TouchableOpacity
             style={styles.searchButton}
             onPress={() => {
-              navigation.navigate('Profile', {
-                hero: Sample,
-              });
+              searchByName(superHeroName);
             }}>
             <Text style={styles.searchButtonText}>Q</Text>
           </TouchableOpacity>
@@ -106,16 +170,50 @@ export function Home({navigation}) {
       {/* 2 */}
       <View style={styles.mainContainer}>
         <View style={styles.cardHolderContainer}>
-          {/* map superheroes */}
-          {superHeroes.map((aSuperHero, cardIndex) => (
-            //  card
-            <View style={styles.cardHolder} key={cardIndex}>
-              <HomeCard
-                SuperHero={aSuperHero}
-                navigateToProfile={() => navigateToProfile(aSuperHero)}
+          <ScrollView
+            contentContainerStyle={styles.scrollView}
+            // handle page refresh (refetch on pull down)
+            refreshControl={
+              <RefreshControl
+                refreshing={isReFetching}
+                onRefresh={invalidateAndRefetch}
               />
-            </View>
-          ))}
+            }>
+            {!isFetchingSuperHeroes ? (
+              // there was an error fetching
+              superHeroFetchError ? (
+                <Text>{superHeroFetchError}</Text>
+              ) : superHeroData.length > 0 ? (
+                // map found superheroes
+                superHeroData.map((aSuperHero, cardIndex) => (
+                  //  card
+                  <View style={styles.cardHolder} key={cardIndex}>
+                    <HomeCard
+                      SuperHero={aSuperHero}
+                      navigateToProfile={() => navigateToProfile(aSuperHero)}
+                    />
+                  </View>
+                ))
+              ) : superHeroesStatus === 'success' ? (
+                // map random superheroes
+                superHeroesData.map((aSuperHero, cardIndex) => (
+                  //  card
+                  <View style={styles.cardHolder} key={cardIndex}>
+                    <HomeCard
+                      SuperHero={aSuperHero}
+                      navigateToProfile={() => navigateToProfile(aSuperHero)}
+                    />
+                  </View>
+                ))
+              ) : (
+                <Text>There was an error fetching. Pull down to refresh</Text>
+              )
+            ) : (
+              !isReFetching && (
+                <ActivityIndicator size="large" color="#0000ff" />
+              )
+            )}
+          </ScrollView>
         </View>
       </View>
     </View>
