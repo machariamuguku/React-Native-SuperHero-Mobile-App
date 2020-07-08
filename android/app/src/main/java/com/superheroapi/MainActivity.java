2bf2b4d40dc236dcaec8ade4f0041cf0ecf24959
Muguku <hello@muguku.co.ke>
2020-07-08 22:41:51 +0300

    added a splash screen

--- android/app/src/main/java/com/superheroapi/MainActivity.java
+++ android/app/src/main/java/com/superheroapi/MainActivity.java
@@ -1,15 +1,25 @@
 package com.superheroapi;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // Import this.
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
     return "SuperHeroApi";
+  }
+
+  // for splash screen
+  // Add this method.
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
